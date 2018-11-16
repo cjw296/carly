4dd66eb7c9a21c8cd7655f20164c8a6b3c6ed355
Chris Withers <chris@simplistix.co.uk>
2018-11-16 13:28:39 +0000

    Factor out setup and teardown helpers into a Context

--- tests/test_line_receiver_server.py
+++ tests/test_line_receiver_server.py
@@ -1,12 +1,10 @@
+from functools import partial
 from testfixtures import compare
-from twisted.internet import protocol, reactor
-from twisted.internet.defer import inlineCallbacks, maybeDeferred
+from twisted.internet.defer import inlineCallbacks
 from twisted.protocols.basic import LineReceiver
 from twisted.trial.unittest import TestCase
 
-from carly import hook, waitUntilAll
-from carly.hook import hookMethod
-
+from carly import Context, hookMethod
 from .line_receiver import EchoServer
 
 
@@ -18,27 +16,12 @@ class TestLineReceiverServer(TestCase):
 
     @inlineCallbacks
     def setUp(self):
-        self.serverProtocolClass = hook(EchoServer, 'connectionLost')
-        f = protocol.Factory()
-        f.protocol = self.serverProtocolClass
-        self.serverPort = reactor.listenTCP(0, f)
-        self.clientProtocolClass = hook(ClientProtocol, 'connectionMade', 'connectionLost')
-        hookMethod(self.clientProtocolClass, 'lineReceived', decoder=lambda line: line)
-        factory = protocol.ClientFactory()
-        factory.protocol = self.clientProtocolClass
-        self.clientConnection = reactor.connectTCP('localhost', self.serverPort.getHost().port, factory)
-        self.client = yield self.clientProtocolClass.connectionMade.protocol()
-
-    @inlineCallbacks
-    def tearDown(self):
-        yield waitUntilAll(
-            maybeDeferred(self.clientConnection.disconnect),
-            self.clientProtocolClass.connectionLost.called(),
-            self.serverProtocolClass.connectionLost.called(),
-        )
-        yield waitUntilAll(
-            maybeDeferred(self.serverPort.stopListening),
-        )
+        context = Context()
+        server = context.makeTCPServer(EchoServer)
+        client = context.makeTCPClient(ClientProtocol, port=server.port)
+        hookMethod(client.protocolClass, 'lineReceived', decoder=lambda line: line)
+        self.client = yield client.protocol
+        self.addCleanup(partial(context.cleanup))
 
     def testConnectDisconnect(self):
         pass
